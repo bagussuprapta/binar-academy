80faa5037a6dd0e7810162b53ed321445d56f095
slickeel <slickeelseek@gmail.com>
2022-03-29 07:21:15 +0800

    fix: bug parameter

--- challenge/ch4-mini-challenge/student.js
+++ challenge/ch4-mini-challenge/student.js
@@ -8,25 +8,25 @@ const student = [
   }),
 ];
 
-function introduce(student, isWestJava, isOver22, isSingle) {
+function introduce(student, isFromWestJava, isOver22, isSingle) {
   let speak = `Nama saya ${student.firstName} ${student.lastName},`;
-  speak += isWestJava() ? " Saya tinggal di Jawa Barat," : " Saya tidak tinggal di Jawa Barat,";
-  speak += isOver22() ? " umur saya diatas 22 tahun." : " umur saya dibawah 22 tahun.";
-  speak += isSingle() ? " Dan status saya single." : " Dan saya tidak single.";
+  speak += isFromWestJava(student.province) ? " Saya tinggal di Jawa Barat," : " Saya tidak tinggal di Jawa Barat,";
+  speak += isOver22(student.age) ? " umur saya diatas 22 tahun." : " umur saya dibawah 22 tahun.";
+  speak += isSingle(student.status) ? " Dan status saya single." : " Dan status saya tidak single.";
   return speak;
 }
 
 console.log(
   introduce(
     student[0],
-    () => {
-      return student.province == "Jawa Barat" ? true : false;
+    (province) => {
+      return province == "Jawa Barat" ? true : false;
     },
-    () => {
-      return student.age > 22 ? true : false;
+    (age) => {
+      return age > 22 ? true : false;
     },
-    () => {
-      return student.status == "single" ? true : false;
+    (status) => {
+      return status == "single" ? true : false;
     }
   )
 );
