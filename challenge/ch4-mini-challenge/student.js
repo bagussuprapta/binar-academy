90abdf519f385581ae222bcac0f8e826f205e5f4
slickeel <slickeelseek@gmail.com>
2022-03-28 23:53:20 +0800

    test: introduce student with callback

--- challenge/ch4-mini-challenge/student.js
+++ challenge/ch4-mini-challenge/student.js
@@ -1,31 +1,32 @@
 const student = [
-  gusde = {
+  (gusde = {
     firstName: "Bagus",
     lastName: "Suprapta",
     province: "Bali",
     age: 23,
     status: "single",
-
-    isWestJava: function() {
-      return this.province == "West Java" ? true : false;
-    },
-    isOver22: function() {
-      return this.age > 22 ? true : false;
-    },
-    isSingle: function() {
-      return this.status == "single" ? true : false;
-    },
-    introduce: function() {
-      let speak = `Nama saya ${this.firstName} ${this.lastName},`;
-      speak += this.isWestJava() ? " Saya tinggal di Jawa Barat," : " Saya tidak tinggal di Jawa Barat,";
-      speak += this.isOver22() ? " umur saya diatas 22 tahun." : " umur saya dibawah 22 tahun.";
-      speak += this.isSingle()? " Dan status saya single." : " Dan saya tidak single.";
-      return speak;
-    }
-  }
+  }),
 ];
 
-console.log(student[0].isWestJava());
-console.log(student[0].isOver22());
-console.log(student[0].isSingle());
-console.log(student[0].introduce());
+function introduce(student, isWestJava, isOver22, isSingle) {
+  let speak = `Nama saya ${student.firstName} ${student.lastName},`;
+  speak += isWestJava() ? " Saya tinggal di Jawa Barat," : " Saya tidak tinggal di Jawa Barat,";
+  speak += isOver22() ? " umur saya diatas 22 tahun." : " umur saya dibawah 22 tahun.";
+  speak += isSingle() ? " Dan status saya single." : " Dan saya tidak single.";
+  return speak;
+}
+
+console.log(
+  introduce(
+    student[0],
+    () => {
+      return student.province == "Jawa Barat" ? true : false;
+    },
+    () => {
+      return student.age > 22 ? true : false;
+    },
+    () => {
+      return student.status == "single" ? true : false;
+    }
+  )
+);
