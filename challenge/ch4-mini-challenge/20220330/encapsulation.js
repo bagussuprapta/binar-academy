c3a362cb0f36d7f60b1eae25e5b280a46448d6ef
slickeel <slickeelseek@gmail.com>
2022-03-30 22:30:18 +0800

    test: encapsulation by facilitator

--- challenge/ch4-mini-challenge/20220330/encapsulation.js
+++ challenge/ch4-mini-challenge/20220330/encapsulation.js
@@ -26,3 +26,90 @@
     console.error(err);
   }
 }
+
+{
+  class Human {
+    constructor(firstName, lastName, gender, state) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.gender = gender;
+      this.state = state;
+    }
+
+    #preparation(){
+      console.log("Bangun tidur");
+      console.log("Mandi");
+      console.log("Sarapan");
+    }
+
+    #doWork(){
+      console.log("Absen");
+      console.log("Kerja");
+      console.log("Istirahat");
+      console.log("Pulang");
+    }
+
+    #getHome(){
+      console.log("Naik kendaraan");
+      console.log("Perjalanan pulang");
+    }
+
+    daily(){
+      this.#preparation();
+      this.#doWork();
+      this.#getHome();
+    }
+
+    _play(){
+      console.log("Main PUBG kuy!");
+    }
+  }
+
+  class Chef extends Human {
+    doPlay(){ 
+      super._play();
+    }
+
+    _doCook(){
+      console.log(`I'm cooking baby!`);
+    }
+
+    #preparation(){
+      console.log(`Trying overriding parent`);
+    }
+
+    callPreparation(){
+      this.#preparation();
+    }
+  }
+
+  const gusde = new Chef("Bagus", "Suprapta", "Male", "Indonesia");
+  gusde.doPlay();
+  gusde._doCook();
+  gusde.callPreparation();
+}
+
+{
+  class GrandFather {
+    constructor(grandFatherName){
+      this.grandFatherName = grandFatherName;
+    }
+  }
+
+  class Father extends GrandFather {
+    constructor(grandFatherName, fatherName){
+      super(grandFatherName);
+      this.fatherName = fatherName;
+    }
+  }
+
+  class Son extends Father {
+    constructor(grandFatherName, fatherName, sonName){
+      super(grandFatherName, fatherName, sonName);
+      this.sonName = sonName;
+    } 
+  }
+
+  const gusde = new Son("Heru", "Here", "Gusde");
+  console.log(gusde);
+}
