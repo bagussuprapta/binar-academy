024656531cd6cd4e2c70e8d33670b60f60e184a0
slickeel <slickeelseek@gmail.com>
2022-04-04 09:31:14 +0800

    test: create a person and save it into json file

--- challenge/ch4-mini-challenge/20220403/index.js
+++ challenge/ch4-mini-challenge/20220403/index.js
@@ -3,6 +3,7 @@ console.log("Hello World");
 const os = require("os");
 const areaOfTriangle = require("./segitiga");
 const fs = require("fs");
+const createPerson = require("./create")
 
 console.log("Free memory: ", os.freemem());
 console.log("Luas segitiga: ", areaOfTriangle(2, 4));
@@ -11,3 +12,11 @@ const value = fs.readFileSync("./note.txt", "utf-8");
 console.log(value);
 
 fs.writeFileSync("./write.txt", "Hello World");
+
+createPerson({
+  name: "Gusde",
+  age: 23,
+  address: "Bali"
+});
+
+console.log(fs.readFileSync("./person.json", "utf-8"));
