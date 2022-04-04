9083b5a1b8ab1faca265e347a9756efe3b9fb2da
slickeel <slickeelseek@gmail.com>
2022-04-04 09:25:11 +0800

    test: read file with fs module

--- challenge/ch4-mini-challenge/20220403/index.js
+++ challenge/ch4-mini-challenge/20220403/index.js
@@ -2,6 +2,10 @@ console.log("Hello World");
 
 const os = require("os");
 const areaOfTriangle = require("./segitiga");
+const fs = require("fs");
 
 console.log("Free memory: ", os.freemem());
 console.log("Luas segitiga: ", areaOfTriangle(2, 4));
+
+const value = fs.readFileSync("./note.txt", "utf-8");
+console.log(value);
