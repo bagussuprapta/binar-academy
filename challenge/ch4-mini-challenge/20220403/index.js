7f9618b237161323fe999fd47f2ec968fd174fca
slickeel <slickeelseek@gmail.com>
2022-04-04 09:21:24 +0800

    test: export and import module

--- challenge/ch4-mini-challenge/20220403/index.js
+++ challenge/ch4-mini-challenge/20220403/index.js
@@ -1,4 +1,7 @@
 console.log("Hello World");
 
 const os = require("os");
+const areaOfTriangle = require("./segitiga");
+
 console.log("Free memory: ", os.freemem());
+console.log("Luas segitiga: ", areaOfTriangle(2, 4));
