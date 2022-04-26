0408a2874fc11e73ba313e8e24148b2744d9800e
gusde <slickeel@icloud.com>
2022-04-26 13:24:31 +0800

    fix: database

--- challenge/ch6_mini-challenge/20220426_service-repository-pattern/config/database.js
+++ challenge/ch6_mini-challenge/20220426_service-repository-pattern/config/database.js
@@ -5,10 +5,10 @@
 
 /** Destruct environment variable to get database configuration */
 const {
-  DB_USERNAME = null,
-  DB_PASSWORD = null,
+  DB_USERNAME = "postgres",
+  DB_PASSWORD = "123",
   DB_HOST = "127.0.0.1",
-  DB_NAME = "database",
+  DB_NAME = "20220426_service-repository-pattern",
 } = process.env;
 
 module.exports = {
