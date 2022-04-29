bd499bc9f8a3054b2268f05f891cfa626622fe2e
gusde <slickeel@icloud.com>
2022-04-29 22:05:11 +0800

    build: sequelize init

---
+++ challenge/ch6_challenge/config/database.js
@@ -0,0 +1,35 @@
+/**
+ * @file Manages database connection configuration.
+ * @author Gusde
+ */
+
+const {
+  DB_USERNAME = "postgres",
+  DB_PASSWORD = "123",
+  DB_HOST = "127.0.0.1",
+  DB_NAME = "database",
+} = process.env;
+
+module.exports = {
+  development: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: `${DB_NAME}_development`,
+    host: DB_HOST,
+    dialect: "postgres",
+  },
+  test: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: `${DB_NAME}_test`,
+    host: DB_HOST,
+    dialect: "postgres",
+  },
+  production: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: `${DB_NAME}_production`,
+    host: DB_HOST,
+    dialect: "postgres",
+  },
+};
