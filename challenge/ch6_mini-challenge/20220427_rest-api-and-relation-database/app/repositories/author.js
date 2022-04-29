14e37912053aa2e2e56e41b956d0b7a4aa6b1a67
gusde <slickeel@icloud.com>
2022-04-29 18:26:47 +0800

    feat: findOne author repositories

--- challenge/ch6_mini-challenge/20220427_rest-api-and-relation-database/app/repositories/author.js
+++ challenge/ch6_mini-challenge/20220427_rest-api-and-relation-database/app/repositories/author.js
@@ -1,7 +1,19 @@
-const {Author} = require("../models");
+const {Author, Post} = require("../models");
 
 module.exports = {
   create(createArgs){
     return Author.create(createArgs);
+  },
+
+  findOne(id){
+    return Author.findOne({
+      where: {
+        id: id
+      },
+      include: {
+        model: Post,
+        attributes: ['title', 'body']
+      }
+    });
   }
 }
