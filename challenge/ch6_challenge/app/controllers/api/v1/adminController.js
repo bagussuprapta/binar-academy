ff1215595816e99bfa19fe6fabc724efc78553f2
gusde <slickeel@icloud.com>
2022-05-07 10:07:51 +0800

    feat(api): login admin

--- challenge/ch6_challenge/app/controllers/api/v1/adminController.js
+++ challenge/ch6_challenge/app/controllers/api/v1/adminController.js
@@ -13,6 +13,19 @@ function encryptPassword(password) {
   });
 }
 
+function checkPassword(password, encryptedPassword) {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, encryptedPassword, (err, isPasswordCorrect) => {
+      if (err) reject(err);
+      return resolve(isPasswordCorrect);
+    });
+  });
+}
+
+function createToken(payload) {
+  return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET);
+}
+
 module.exports = {
   async create(req, res, err) {
     try {
@@ -29,6 +42,25 @@ module.exports = {
       res.status(422).json({
         status: "Unprocessable Entity",
       });
+    }
+  },
+
+  async login(req, res) {
+    try {
+      const admin = await Admins.findOne({
+        where: { username: req.body.username },
+      });
+      if (!admin) res.status(404).json({ status: "Not Found" });
+      if (!(await checkPassword(req.body.password, admin.password)))
+        res.status(401).json("Unauthorized");
+
+      const token = createToken({
+        id: admin.id,
+        username: admin.username,
+      });
+      res.status(202).json({ status: "Accepted", token});
+    } catch (err) {
+      res.status(400).json({ err });
     }
   },
 };
--- challenge/ch6_challenge/routes/apiRouter.js
+++ challenge/ch6_challenge/routes/apiRouter.js
@@ -9,5 +9,6 @@ router.get("/v1/cars", controllers.api.v1.carController.list);
 router.post("/v1/users", controllers.api.v1.userController.create);
 
 router.post("/v1/admins", controllers.api.v1.adminController.create);
+router.post("/v1/admins/login", controllers.api.v1.adminController.login);
 
 module.exports = router;
