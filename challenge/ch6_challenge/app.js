1813faded848e98631c42af0ece54e37e3be2e16
gusde <slickeel@icloud.com>
2022-05-13 23:43:00 +0800

    feat(api): user api documentation

--- challenge/ch6_challenge/app.js
+++ challenge/ch6_challenge/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 const mainRouter = require("./routes/mainRouter");
 const userRouter = require("./routes/userRouter");
