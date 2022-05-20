8dfe38d9d00295e69a120ef2897af6535140f1a7
gusde <slickeel@icloud.com>
2022-05-20 21:07:41 +0800

    feat: count number when clicked

--- challenge/ch7_mini-challenge/20220518_state/src/component/App.js
+++ challenge/ch7_mini-challenge/20220518_state/src/component/App.js
@@ -1,14 +1,18 @@
 import React from "react";
-import "./App.css"
+import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name: "Gusde" };
+    this.state = { name: "Gusde", number: 0 };
   }
 
-  nameClick(name){
-    this.setState({name: name})
+  nameClick(name) {
+    this.setState({ name: name });
+  }
+
+  countClick() {
+    this.setState({ number: this.state.number + 1});
   }
 
   render() {
@@ -16,8 +20,22 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1>Halo, {this.state.name}</h1>
-        <button onClick={() => {this.nameClick("Gusde")}}>Gusde</button>
-        <button onClick={() => {this.nameClick("FSW-13")}}>FSW-13</button>
+        <button
+          onClick={() => {
+            this.nameClick("Gusde");
+          }}
+        >
+          Gusde
+        </button>
+        <button
+          onClick={() => {
+            this.nameClick("FSW-13");
+          }}
+        >
+          FSW-13
+        </button>
+        <h1>{this.state.number}</h1>
+        <button onClick={() => {this.countClick()}}>+</button>
       </div>
     );
   }
