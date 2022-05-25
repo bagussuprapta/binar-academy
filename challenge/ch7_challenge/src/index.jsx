7505969eb0f545083ca6a6a502d4047a04e69fc0
gusde <slickeel@icloud.com>
2022-05-25 15:28:34 +0800

    build: initiate react project

---
+++ challenge/ch7_challenge/src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+import './index.css';
+import Navbar from "./components/Navbar";
+import Introduction from "./components/Introduction"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  </React.StrictMode>
+);
