e5af96bcde0d5e09d59155acf072650f3f601017
gusde <slickeel@icloud.com>
2022-05-27 13:06:03 +0800

    style: why us component

---
+++ challenge/ch7_challenge/src/components/WhyUs.jsx
@@ -0,0 +1,82 @@
+function WhyUs() {
+  return (
+    <article className="container mt-5" id="why-us">
+      <div className="row why-us">
+        <div className="col-12 why-us-title">
+          <h2 className="">Why Us?</h2>
+          <p>Why should you choose Binar Car Rental?</p>
+        </div>
+        <div className="col-12 why-us-card">
+          <div className="row justify-content-center">
+            <div className="col-md-6 col-lg-3 why-us-card-item">
+              <div className="card">
+                <div className="card-body">
+                  <img
+                    src="https://img.icons8.com/emoji/8x/automobile.png"
+                    alt=""
+                    width="30px"
+                  />
+                  <h5 className="card-title">All Cars Available</h5>
+                  <p className="card-text">
+                    There are many choices of cars, the condition is new, clean
+                    and well-maintained
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-3 why-us-card-item">
+              <div className="card">
+                <div className="card-body">
+                  <img
+                    src="https://img.icons8.com/emoji/8x/dollar-banknote-emoji.png"
+                    alt=""
+                    width="30px"
+                  />
+                  <h5 className="card-title">Cheap Price</h5>
+                  <p className="card-text">
+                    Cheap and competitive prices, you can compare our prices
+                    with other car rentals
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-3 why-us-card-item">
+              <div className="card">
+                <div className="card-body">
+                  <img
+                    src="https://img.icons8.com/emoji/8x/convenience-store.png"
+                    alt=""
+                    width="30px"
+                  />
+                  <h5 className="card-title">24 Hours Service</h5>
+                  <p className="card-text">
+                    Ready to serve your needs 24 hours non-stop. We are also
+                    available on weekends
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-3 why-us-card-item">
+              <div className="card">
+                <div className="card-body">
+                  <img
+                    src="https://img.icons8.com/emoji/2x/man-with-beard-medium-skin-tone.png"
+                    alt=""
+                    width="30px"
+                  />
+                  <h5 className="card-title">Pro Driver</h5>
+                  <p className="card-text">
+                    Professional, experienced, honest, friendly, and of course
+                    always on time driver{" "}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default WhyUs;
