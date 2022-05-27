ea0fcb0a7e2deb92fbd3ad11dfd54ae4bb8c6d44
gusde <slickeel@icloud.com>
2022-05-27 13:02:44 +0800

    style: change introduction image and add border radius

--- challenge/ch7_challenge/src/components/Introduction.jsx
+++ challenge/ch7_challenge/src/components/Introduction.jsx
@@ -1,26 +1,26 @@
-function Introduction({srcImage}) {
+import "./Introduction.css"
+
+function Introduction({ srcImage }) {
   return (
-    <article class="container-fluid introduction pt-5">
-      <div class="row align-items-center intro">
-        <div class="col-md-5 offset-md-1 intro-text">
+    <article className="container-fluid introduction pt-3">
+      <div className="row align-items-center intro">
+        <div className="col-sm-6 col-md-5 offset-md-1 intro-text">
           <div>
-            <h2>
-              Sewa & Rental Mobil Terbaik <br /> di kawasan Denpasar
-            </h2>
+            <h2>Best Car Rental in Bali</h2>
           </div>
           <div>
             <p>
-              Selamat datang di Binar Car Rental. Kami menyediakan mobil
-              kualitas <br /> terbaik dengan harga terjangkau. Selalu siap
-              melayani kebutuhanmu <br /> untuk sewa mobil selama 24 jam.
+              Welcome to Binar Car Rental. We provide the best quality cars at <br />
+              affordable prices. Always ready to serve your needs for car rental <br />
+              for 24 hours.
             </p>
           </div>
           <div>
-            <button class="btn btn-success">Start Car Rental</button>
+            <a className="btn btn-primary" href="/cars">Start Car Rental</a>
           </div>
         </div>
-        <div class="col-md-6 intro-img">
-          <img class="img-fluid" src="./images/img_car.png" alt="car avanza"/>
+        <div className="col-sm-6 col-md-6 intro-img">
+          <img className="img-fluid" src="./images/drawkit-call.svg" alt="car avanza" />
         </div>
       </div>
     </article>
