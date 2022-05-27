8197c1e88258dacce29929279f8e8b798d33e47d
gusde <slickeel@icloud.com>
2022-05-27 13:07:30 +0800

    fix: route

--- challenge/ch7_challenge/src/index.jsx
+++ challenge/ch7_challenge/src/index.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.min.js"
-import './index.css';
-import Navbar from "./components/Navbar";
-import Introduction from "./components/Introduction"
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./index.css";
+import LandingPage from "./pages/LandingPage";
+import FindCar from "./pages/FindCar";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Navbar />
-    <Introduction
-      srcImage="assets/image/img_car.png"
-    />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/cars" element={<FindCar />} />
+    </Routes>
+  </BrowserRouter>
 );
